7f9647621a52167627cf7efae250c36680752869
Imaolabiyi <sheron4krist@yahoo.com>
2024-08-06 13:43:53 +0000

    ... but why??

--- markdown2html.py
+++ markdown2html.py
@@ -1,121 +1,86 @@
 #!/usr/bin/python3
 """
-Module to convert Markdown to HTML
+Markdown to HTML script with additional parsing for custom bold syntax.
 """
-
 import sys
-import os
 import re
+import hashlib
 
-def convert_text_formatting(text):
-    """
-    Convert Markdown bold (**text**) and emphasis (__text__) to HTML.
-    """
-    text = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', text)
-    text = re.sub(r'__(.*?)__', r'<em>\1</em>', text)
-    return text
+def md5_hash(content):
+    return hashlib.md5(content.encode()).hexdigest()
 
-def markdown_to_html(markdown_content):
-    html_content = []
-    in_unordered_list = False
-    in_ordered_list = False
-    in_paragraph = False
-    paragraph_lines = []
+def remove_c(content):
+    return re.sub(r'c', '', content, flags=re.IGNORECASE)
 
-    for line in markdown_content:
-        line = line.rstrip()  # strip trailing spaces but keep leading spaces for indentation
-        header_match = re.match(r'^(#{1,6})\s+(.*)', line)
-        unordered_list_match = re.match(r'^-\s+(.*)', line)
-        ordered_list_match = re.match(r'^\*\s+(.*)', line)
-
-        if header_match:
-            if in_unordered_list:
-                html_content.append('</ul>')
-                in_unordered_list = False
-            if in_ordered_list:
-                html_content.append('</ol>')
-                in_ordered_list = False
-            if in_paragraph:
-                html_content.append('<br/>'.join(paragraph_lines))
-                html_content.append('</p>')
-                in_paragraph = False
-                paragraph_lines = []
-            level = len(header_match.group(1))
-            html_content.append('<h{level}>{text}</h{level}>'.format(level=level, text=convert_text_formatting(header_match.group(2))))
-        elif unordered_list_match:
-            if in_ordered_list:
-                html_content.append('</ol>')
-                in_ordered_list = False
-            if in_paragraph:
-                html_content.append('<br/>'.join(paragraph_lines))
-                html_content.append('</p>')
-                in_paragraph = False
-                paragraph_lines = []
-            if not in_unordered_list:
-                html_content.append('<ul>')
-                in_unordered_list = True
-            html_content.append('<li>{}</li>'.format(convert_text_formatting(unordered_list_match.group(1))))
-        elif ordered_list_match:
-            if in_unordered_list:
-                html_content.append('</ul>')
-                in_unordered_list = False
-            if in_paragraph:
-                html_content.append('<br/>'.join(paragraph_lines))
-                html_content.append('</p>')
-                in_paragraph = False
-                paragraph_lines = []
-            if not in_ordered_list:
-                html_content.append('<ol>')
-                in_ordered_list = True
-            html_content.append('<li>{}</li>'.format(convert_text_formatting(ordered_list_match.group(1))))
-        elif line:
-            if in_unordered_list:
-                html_content.append('</ul>')
-                in_unordered_list = False
-            if in_ordered_list:
-                html_content.append('</ol>')
-                in_ordered_list = False
-            if not in_paragraph:
-                html_content.append('<p>')
-                in_paragraph = True
-            paragraph_lines.append(convert_text_formatting(line))
+def parse_markdown_to_html(markdown_text):
+    html_lines = []
+    in_list = False
+    paragraphs = []
+    
+    for line in markdown_text.split('\n'):
+        line = line.strip()
+        
+        if not line:
+            if in_list:
+                html_lines.append('</ul>')
+                in_list = False
+            if paragraphs:
+                html_lines.append('<p>' + '<br/>\n'.join(paragraphs) + '</p>')
+                paragraphs = []
+            continue
+        
+        # Heading
+        if line.startswith('# '):
+            if paragraphs:
+                html_lines.append('<p>' + '<br/>\n'.join(paragraphs) + '</p>')
+                paragraphs = []
+            html_lines.append(f'<h1>{line[2:].strip()}</h1>')
+        elif line.startswith('- '):
+            if paragraphs:
+                html_lines.append('<p>' + '<br/>\n'.join(paragraphs) + '</p>')
+                paragraphs = []
+            if not in_list:
+                in_list = True
+                html_lines.append('<ul>')
+            # Bold with ** inside list
+            line = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', line[2:].strip())
+            html_lines.append(f'<li>{line}</li>')
         else:
-            if in_paragraph:
-                html_content.append('<br/>'.join(paragraph_lines))
-                html_content.append('</p>')
-                in_paragraph = False
-                paragraph_lines = []
+            # Bold with **
+            line = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', line)
+            # Italic with __
+            line = re.sub(r'__(.*?)__', r'<em>\1</em>', line)
+            # MD5 hash with [[...]]
+            line = re.sub(r'\[\[(.*?)\]\]', lambda match: md5_hash(match.group(1)), line)
+            # Remove 'c' with ((...))
+            line = re.sub(r'\(\((.*?)\)\)', lambda match: remove_c(match.group(1)), line)
+            paragraphs.append(line)
+    
+    if in_list:
+        html_lines.append('</ul>')
+    if paragraphs:
+        html_lines.append('<p>' + '<br/>\n'.join(paragraphs) + '</p>')
 
-    if in_unordered_list:
-        html_content.append('</ul>')
-    if in_ordered_list:
-        html_content.append('</ol>')
-    if in_paragraph:
-        html_content.append('<br/>'.join(paragraph_lines))
-        html_content.append('</p>')
-
-    return '\n'.join(html_content)
-
-def main():
-    if len(sys.argv) != 3:
-        print("Usage: ./markdown2html.py README.md README.html")
-        sys.exit(1)
-
-    markdown_file = sys.argv[1]
-    html_file = sys.argv[2]
-
-    if not os.path.isfile(markdown_file):
-        print("Missing {}".format(markdown_file))
-        sys.exit(1)
-
-    with open(markdown_file, 'r') as f:
-        markdown_content = f.readlines()
-
-    html_content = markdown_to_html(markdown_content)
-
-    with open(html_file, 'w') as f:
-        f.write(html_content)
+    return '\n'.join(html_lines)
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html")
+        sys.exit(1)
+    
+    markdown_file = sys.argv[1]
+    html_file = sys.argv[2]
+    
+    try:
+        with open(markdown_file, 'r') as md_file:
+            markdown_text = md_file.read()
+        
+        html_content = parse_markdown_to_html(markdown_text)
+        
+        with open(html_file, 'w') as html_file:
+            html_file.write(html_content)
+    
+    except Exception as e:
+        print(f"Error: {e}")
+        sys.exit(1)
 
