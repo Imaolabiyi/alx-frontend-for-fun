4d84a7fc65321e49fee9a04baeb9f83e40ce2816
Imaolabiyi <sheron4krist@yahoo.com>
2024-08-06 08:37:40 +0000

    Simple text

--- markdown2html.py
+++ markdown2html.py
@@ -11,13 +11,15 @@ def markdown_to_html(markdown_content):
     html_content = []
     in_unordered_list = False
     in_ordered_list = False
+    in_paragraph = False
+    paragraph_lines = []
 
     for line in markdown_content:
-        line = line.strip()
+        line = line.rstrip()  # strip trailing spaces but keep leading spaces for indentation
         header_match = re.match(r'^(#{1,6})\s+(.*)', line)
         unordered_list_match = re.match(r'^-\s+(.*)', line)
         ordered_list_match = re.match(r'^\*\s+(.*)', line)
-        
+
         if header_match:
             if in_unordered_list:
                 html_content.append('</ul>')
@@ -25,12 +27,22 @@ def markdown_to_html(markdown_content):
             if in_ordered_list:
                 html_content.append('</ol>')
                 in_ordered_list = False
+            if in_paragraph:
+                html_content.append('<br/>'.join(paragraph_lines))
+                html_content.append('</p>')
+                in_paragraph = False
+                paragraph_lines = []
             level = len(header_match.group(1))
             html_content.append('<h{level}>{text}</h{level}>'.format(level=level, text=header_match.group(2)))
         elif unordered_list_match:
             if in_ordered_list:
                 html_content.append('</ol>')
                 in_ordered_list = False
+            if in_paragraph:
+                html_content.append('<br/>'.join(paragraph_lines))
+                html_content.append('</p>')
+                in_paragraph = False
+                paragraph_lines = []
             if not in_unordered_list:
                 html_content.append('<ul>')
                 in_unordered_list = True
@@ -39,24 +51,40 @@ def markdown_to_html(markdown_content):
             if in_unordered_list:
                 html_content.append('</ul>')
                 in_unordered_list = False
+            if in_paragraph:
+                html_content.append('<br/>'.join(paragraph_lines))
+                html_content.append('</p>')
+                in_paragraph = False
+                paragraph_lines = []
             if not in_ordered_list:
                 html_content.append('<ol>')
                 in_ordered_list = True
             html_content.append('<li>{}</li>'.format(ordered_list_match.group(1)))
-        else:
+        elif line:
             if in_unordered_list:
                 html_content.append('</ul>')
                 in_unordered_list = False
             if in_ordered_list:
                 html_content.append('</ol>')
                 in_ordered_list = False
-            if line:  # To avoid adding empty lines
-                html_content.append(line)
+            if not in_paragraph:
+                html_content.append('<p>')
+                in_paragraph = True
+            paragraph_lines.append(line)
+        else:
+            if in_paragraph:
+                html_content.append('<br/>'.join(paragraph_lines))
+                html_content.append('</p>')
+                in_paragraph = False
+                paragraph_lines = []
 
     if in_unordered_list:
         html_content.append('</ul>')
     if in_ordered_list:
         html_content.append('</ol>')
+    if in_paragraph:
+        html_content.append('<br/>'.join(paragraph_lines))
+        html_content.append('</p>')
 
     return '\n'.join(html_content)
 
